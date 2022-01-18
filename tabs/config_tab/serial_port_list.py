cf2eb39fd80cfed4833d7f34f0cc79a9e2f624fc
AaronLi <dumfing@gmail.com>
2022-01-18 18:12:19 -0500

    Change config to display more detailed serial port info

--- tabs/config_tab/serial_port_list.py
+++ tabs/config_tab/serial_port_list.py
@@ -1,3 +1,4 @@
+from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QWidget, QHBoxLayout, QComboBox, QPushButton
 from serial.tools import list_ports
 
@@ -8,22 +9,21 @@ class SerialPortList(QWidget):
         self.layout = QHBoxLayout()
 
         self.options = QComboBox()
-        for port in list_ports.comports():
-            self.options.addItem(port.name)
         self.layout.addWidget(self.options)
 
         refresh_button = QPushButton("Refresh")
         refresh_button.clicked.connect(self.refresh)
         self.layout.addWidget(refresh_button)
         self.setLayout(self.layout)
+        self.refresh()
 
     def refresh(self):
         self.options.clear()
         for port in list_ports.comports():
-            self.options.addItem(port.name)
+            self.options.addItem(port.description, port.name)
 
     def is_port_selected(self):
         return self.options.currentText() != ""
 
     def get_selected_port(self):
-        return self.options.currentText()
+        return self.options.currentData(Qt.UserRole)
