b90d0d1c3636376a54476ea93c2883fd8d394fe3
Jan <janfiedler812@gmail.com>
2022-01-18 23:35:11 -0500

    interface updates, can now add measurement tabs

--- MainWin.py
+++ MainWin.py
@@ -22,7 +22,7 @@ class MainWindow:
         self.ui.tabWidget.setTabText(self.ui.tabWidget.indexOf(self.configTab),QtCore.QCoreApplication.translate("MainWindow", "Config"))
 
 
-        self.algometer_widget = AlgometerReadingGraph(self.ui.default_tab)
+        self.algometer_widget = AlgometerReadingGraph(self.ui.measurementsTab)
         self.algometer_widget.setGeometry(QtCore.QRect(400, 20, 200, 200))
         self.algometer_widget.setObjectName("centralwidget")
 
@@ -34,6 +34,31 @@ class MainWindow:
         #self.ui.noButton.clicked.connect(self.on_no_button_clicked)
         self.ui.PrintButton.clicked.connect(self.on_print_button_clicked)
 
+        self.ui.add_measurement_button.clicked.connect(self.on_measure_area_add)
+
+
+
+        ##keeping track of some data here
+        self.measurement_areas_list = []
+
+
+
+
+    def add_measurement_tab(self):
+        self.newTab = QWidget()
+        new_area = self.ui.comboBox.itemText(self.ui.comboBox.currentIndex())
+        if new_area in self.measurement_areas_list:
+            new_area += " #" + self.measurement_areas_list.count(new_area)
+            print("here")
+        self.newTab.setObjectName(new_area)
+        self.ui.measurements_tabs.addTab(self.newTab, "")
+        self.ui.measurements_tabs.setTabText(self.ui.measurements_tabs.indexOf(self.newTab),
+                                     QtCore.QCoreApplication.translate("MainWindow", new_area))
+
+
+    def on_measure_area_add(self):
+        self.ui.listWidget.addItem(self.ui.comboBox.itemText(self.ui.comboBox.currentIndex()))
+        self.add_measurement_tab()
 
 
     def show(self):
