fb4ae2714d57dc8859939f1655853e50e56ceb35
Jan <janfiedler812@gmail.com>
2022-02-03 18:35:33 -0500

    analysis template added, add graphs where left_graph and right_graph are

--- tabs/analysis_tab.py
+++ tabs/analysis_tab.py
@@ -1,9 +1,29 @@
-from PyQt5.QtWidgets import QWidget
+from PyQt5.QtWidgets import QWidget, QGraphicsView, QLabel, QGraphicsView
+from PyQt5.QtCore import QRect, QCoreApplication
 
 
 class AnalysisTab(QWidget):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+        self.setObjectName("analysisTab")
+        self.left_graph_1 = QGraphicsView(self)
+        self.left_graph_1.setGeometry(QRect(40, 90, 256, 192))
+        self.left_graph_1.setObjectName("left_graph_1")
+        self.patient_name_label = QLabel(self)
+        self.patient_name_label.setGeometry(QRect(60, 40, 231, 20))
+        self.patient_name_label.setObjectName("patient_name_label")
+        self.left_quartile_1 = QLabel(self)
+        self.left_quartile_1.setGeometry(QRect(310, 140, 81, 20))
+        self.left_quartile_1.setObjectName("left_quartile_1")
+        self.right_quartile_1 = QLabel(self)
+        self.right_quartile_1.setGeometry(QRect(690, 140, 81, 20))
+        self.right_quartile_1.setObjectName("right_quartile_1")
+        self.right_graph_1 = QGraphicsView(self)
+        self.right_graph_1.setGeometry(QRect(420, 90, 256, 192))
+        self.right_graph_1.setObjectName("right_graph_1")
+
     def retranslateUi(self):
-        pass
+        self.patient_name_label.setText(QCoreApplication.translate("MainWindow", "Patient Name Here"))
+        self.left_quartile_1.setText(QCoreApplication.translate("MainWindow", "xth quartile"))
+        self.right_quartile_1.setText(QCoreApplication.translate("MainWindow", "xth quartile"))
